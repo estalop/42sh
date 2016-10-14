87ea853c576c025b2c7fba6fed8482de44af5deb
Thomas VIVIAND <tviviand@e1r6p15.42.fr>
2016-10-14 15:44:09 +0200

    merde

--- dlist_new.c
+++ dlist_new.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dlist_new.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pbourdon <pbourdon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/08 15:35:22 by pbourdon          #+#    #+#             */
-/*   Updated: 2016/10/05 15:08:10 by pbourdon         ###   ########.fr       */
+/*   Updated: 2016/10/14 15:41:17 by tviviand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_dlist		*dlist_new(t_dlist *p_new)
 {
-	p_new = malloc(sizeof(*p_new));
+	p_new = malloc(sizeof(t_dlist));
 	if (p_new != NULL)
 	{
 		p_new->length = 0;
