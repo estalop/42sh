035bd85bf1f302afb1a4c4a697084609ad24f7fe
Jean-yves BOBIN <jbobin@e1r6p16.42.fr>
2016-11-07 15:33:02 +0100

    libft update : ft_strsplit moins deg

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: jbobin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 14:12:30 by jbobin            #+#    #+#             */
-/*   Updated: 2016/03/07 08:33:30 by jbobin           ###   ########.fr       */
+/*   Updated: 2016/11/07 15:29:58 by jbobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,41 +28,9 @@ static int		ft_count(char const *s, char c)
 		if (s[i] == c && s[i - 1] != c)
 			j++;
 	}
-	if (i != 0)
+	if (s[i - 1] != c)
 		j++;
 	return (j);
-}
-
-static int		ft_counts(char const *s, char c, int i)
-{
-	int j;
-
-	j = 0;
-	while (s[i] != c && s[i] != '\0')
-	{
-		i++;
-		j++;
-	}
-	return (j + 1);
-}
-
-static	char	*ft_fill(char const *s, char c, int i)
-{
-	char	*t;
-	int		k;
-
-	k = 0;
-	t = (char*)malloc(sizeof(char) * ft_counts(s, c, i));
-	if (t == NULL)
-		return (NULL);
-	while (s[i] != '\0' && s[i] != c)
-	{
-		t[k] = s[i];
-		i++;
-		k++;
-	}
-	t[k] = '\0';
-	return (t);
 }
 
 char			**ft_strsplit(char const *s, char c)
@@ -77,19 +45,17 @@ char			**ft_strsplit(char const *s, char c)
 	j = ft_count(s, c);
 	if ((t = (char**)malloc(sizeof(char*) * (j + 1))) == NULL || j < 0)
 		return (NULL);
-	while (l < j)
+	while (l < ft_count(s, c))
 	{
 		while (s[i] == c)
 			i++;
-		if ((t[l] = ft_fill(s, c, i)) == NULL)
-			return (NULL);
+		j = i;
 		while (s[i] != '\0' && s[i] != c)
 			i++;
+		if ((t[l] = ft_strsub(s, j, i - j)) == NULL)
+			return (NULL);
 		l++;
 	}
-	if (s[i - 1] == c)
-		t[l - 1] = NULL;
-	else
-		t[l] = NULL;
+	t[l] = NULL;
 	return (t);
 }
--- libft/ft_strsub.c
+++ libft/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: jbobin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 11:37:37 by jbobin            #+#    #+#             */
-/*   Updated: 2016/10/05 16:06:17 by pbourdon         ###   ########.fr       */
+/*   Updated: 2016/11/07 15:29:00 by jbobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	unsigned int	i;
 
 	i = 0;
-	if (s == NULL || start >= len)
+	if (s == NULL)
 		return (NULL);
 	str = (char*)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
