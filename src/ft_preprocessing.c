e11e2485653607d53b3219dd8bb25e45dcb7d5b9
Jean-yves BOBIN <jbobin@e1r6p21.42.fr>
2016-11-29 17:48:33 +0100

    YYYYYYYYYYYYOOOOOOOOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLPOOOOOOOOOOOOOO \o/

--- src/ft_preprocessing.c
+++ src/ft_preprocessing.c
@@ -6,7 +6,7 @@
 /*   By: jbobin <jbobin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 12:52:02 by jbobin            #+#    #+#             */
-/*   Updated: 2016/11/29 16:48:39 by jbobin           ###   ########.fr       */
+/*   Updated: 2016/11/29 17:47:09 by jbobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,32 +71,25 @@ static void	ft_process(char *buf, t_prstruct *process, char **path, \
 	signal(2, &ft_signal_stop);
 }
 
-char		**ft_preprocesssplit(char *l, t_operators *t)
+static char	**ft_preprocesssplit(char *l, t_operators *t)
 {
 	char	**com;
 	int		i;
 	int		j;
 	int		c;
 
-	//DECLARATIONS
 	j = 0;
 	com = NULL;
 	i = 1;
 	c = 0;
-	//ANTI CRASH
 	if (!l)
 		return (NULL);
-	//COMPTE LE NOMBRE DE SPLITS THEORIQUES STOCK DANS I
 	i += ft_strcnts(l, "&&");
 	i += ft_strcnts(l, "||");
 	i += ft_strcnt(l, ';');
-	//ALLOCATIONS DU TABLEAU COM CONTENANT LES COMMANDES ET OPERATEURS
 	if (!(com = ft_memalloc((sizeof(char **) * i) + 1)))
 		return (NULL);
-	//REMISE A ZERO DE I POUR REUTILISATION
 	i = 0;
-	//Revoir le systeme de split pour prendre plus de trucs a virer que \t
-	//BOUCLE PRINCIPALE
 	while (l[i])
 	{
 		if ((!ft_strncmp(&l[i], "&&", 2) && ft_strncmp(&l[i], "&&&", 3)) || \
@@ -127,55 +120,26 @@ void		ft_preprocess(char **tmp, t_prstruct *proc, char **path, \
 {
 	t_operators	t;
 
-	//CETTE PARTIE DU CODE N A PAS ETE NI OPTI NI MODIFIEE GRANDEMENT AVANT AJOUT DE LA FONCTION DESSUS!
-
-	//MISE A NULL DE LA STRUCTURE
 	ft_bzero(&t, sizeof(t_operators));
-	//APPEL DE LA FONCTION AU DESSUS
 	t.com = ft_preprocesssplit(*tmp, &t);
-	ft_print_tab(t.com);
 	ft_strdel(tmp);
-	//TANT QUE PAS NULL ET PAS ERREUR
+	if (t.err == -1)
+	{
+		ft_free_tab(&t.com);
+		return ;
+	}
 	while (t.com != NULL && t.com[t.j] != NULL && !t.err)
 	{
-		//DETECTE SI OPERATEUR DETECTE
-		!ft_strcmp(t.com[t.j], "&&") ? t.op = 1 : t.j;
-		!ft_strcmp(t.com[t.j], "||") ? t.op = 2 : t.j;
-		!ft_strcmp(t.com[t.j], ";") ? t.op = 3 : t.j;
-		//A faite : Rajout des autres trucs a elimine pendant le split
-		//RAJOUT DANS LE TABLEAU DES STRINGS ?
-		if (t.com[t.j][0] != '\t' && t.com[t.j][0] != '\n' &&
-		ft_strcmp(t.com[t.j], "&&") && ft_strcmp(t.com[t.j], "||") &&
-		ft_strcmp(t.com[t.j], ";"))
+		if ((t.com[t.j][0] == '&' && proc->stat_lock == 0) || \
+	(t.com[t.j][0] == '|' && proc->stat_lock != 0 && proc->stat_lock < 128) || \
+			(t.com[t.j][0] != '&' && t.com[t.j][0] != '|'))
 		{
-			//SI PAS OPERATEUR EST DETECTE CA RENTRE
-			t.l ? t.l = ft_strjoind(t.l, " ", 1, 0) : t.l;
-			t.l ? t.l = ft_strjoind(t.l, t.com[t.j], 1, 0) : t.l;
-			!t.l ? t.l = ft_strdup(t.com[t.j]) : t.l;
-			if (!t.l)
-				break ;
-			//VERIFIE SI OPERATEUR APRES
-			if (t.com[t.j + 1])
-			{
-				if (!ft_strcmp(t.com[t.j + 1], "&&") ||
-				!ft_strcmp(t.com[t.j + 1], "||") ||
-				!ft_strcmp(t.com[t.j + 1], ";"))
-				{
-					//SI UN OPERATEUR EST DETECTE
-					if (t.op == 3 || (t.op == 1 && proc->stat_lock == 0) ||
-					(t.op == 2 && proc->stat_lock != 0) || t.op == 0)
-						ft_process(t.l, proc, path, heredoc);
-					t.op = 0;
-				ft_strdel(&t.l);
-				}
-			}
-			// ft_putnbr(proc->stat_lock);
+			if ((t.com[t.j][0] == '&' && proc->stat_lock == 0) || \
+		(t.com[t.j][0] == '|' && proc->stat_lock != 0 && proc->stat_lock < 128))
+				ft_process(&t.com[t.j][1], proc, path, heredoc);
+			else
+				ft_process(t.com[t.j], proc, path, heredoc);
+			t.j++;
 		}
-		//LANCE LA DERNIERE COMMANDE ENTREE
-		!t.com[t.j + 1] ? ft_process(t.l, proc, path, heredoc) : t;
-		t.j++;
 	}
-	//FREE
-	t.l ? ft_strdel(&t.l) : t;
-//	ft_free_tab(&t.com);
 }
