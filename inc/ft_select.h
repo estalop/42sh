559e2fdc1f8b7c6315c2223cb85cbc3efb13062e
Thomas VIVIAND <tviviand@e2r2p9.42.fr>
2016-12-08 17:56:19 +0100

    Reviens la erreur de norme, tu est faite !

--- inc/ft_select.h
+++ inc/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: tbayet <tbayet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/11 16:09:58 by tbayet            #+#    #+#             */
-/*   Updated: 2016/12/06 20:41:49 by tviviand         ###   ########.fr       */
+/*   Updated: 2016/12/08 17:55:40 by tviviand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 # include "autocompletion.h"
 # define MAX_SELECT_LINES 8
 
-typedef struct	s_ldim
+typedef struct		s_ldim
 {
-	int			x;
-	int			y;
-	int			pos;
-	int			size;
-	int			scroll;
-	int			maxlen;
-}				t_ldim;
+	int				x;
+	int				y;
+	int				pos;
+	int				size;
+	int				scroll;
+	int				maxlen;
+}					t_ldim;
 
 typedef struct		s_selprintlist
 {
@@ -38,17 +38,17 @@ typedef struct		s_selprintlist
 	char			*line;
 }					t_selprintlist;
 
-int				is_spec_separator(char c);
-char			*ft_select(char **list, t_termcaps *tc, char *line);
-void			ft_select_cancel(char **list, t_termcaps *tc, char *line);
-char			*ft_select_get(char **list, t_termcaps *tc, char **line);
-void			ft_select_move(char **list, char dir, t_termcaps *tc, \
+int					is_spec_separator(char c);
+char				*ft_select(char **list, t_termcaps *tc, char *line);
+void				ft_select_cancel(char **list, t_termcaps *tc, char *line);
+char				*ft_select_get(char **list, t_termcaps *tc, char **line);
+void				ft_select_move(char **list, char dir, t_termcaps *tc, \
 	char *line);
-t_ldim			*ft_select_printlist(char **list, t_termcaps *tc, \
+t_ldim				*ft_select_printlist(char **list, t_termcaps *tc, \
 	t_ldim *dims, char *line);
-void			ft_select_move_up(t_ldim *dims);
-void			ft_select_move_down(t_ldim *dims);
-void			ft_select_move_left(t_ldim *dims);
-void			ft_select_move_right(t_ldim *dims);
+void				ft_select_move_up(t_ldim *dims);
+void				ft_select_move_down(t_ldim *dims);
+void				ft_select_move_left(t_ldim *dims);
+void				ft_select_move_right(t_ldim *dims);
 
 #endif
