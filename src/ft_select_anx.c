8fd170f0b5e089584b875464da76ffb7188a6fc4
Thomas BAYET <tbayet@e2r11p21.42.fr>
2016-12-14 13:51:30 +0100

    completion 100% ok

--- src/ft_select_anx.c
+++ src/ft_select_anx.c
@@ -6,7 +6,7 @@
 /*   By: tviviand <tviviand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 17:30:54 by tviviand          #+#    #+#             */
-/*   Updated: 2016/12/08 17:39:33 by tviviand         ###   ########.fr       */
+/*   Updated: 2016/12/14 13:48:29 by tbayet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,25 @@
 
 int	ft_select_get_anx(t_selectget *s, char **line, char **list, t_termcaps *tc)
 {
+	int len;
+
+	len = ft_strlen(s->res);
 	if (!(s->newwline = ft_strnew(ft_strlen(*line) -
-	(tc->x - tc->prompt - s->i) + ft_strlen(s->res))))
+	(tc->x - tc->neg - s->i) + len)))
 		return (1);
 	s->newwline = ft_strncpy(s->newwline, *line, s->i);
 	s->ptr = s->newwline + s->i;
 	s->ptr = ft_strcpy(s->ptr, s->res);
-	s->ptr = s->newwline + s->i + ft_strlen(s->res);
-	s->ptr = ft_strcpy(s->ptr, (*line) + (tc->x - tc->prompt));
+	s->ptr = s->newwline + s->i + len;
+	s->ptr = ft_strcpy(s->ptr, (*line) + (tc->x - tc->neg));
 	ft_select_cancel(list, tc, *line);
 	ft_thome(tc, *line);
 	tputs(tc->cd, 1, ft_output);
 	ft_newputstr(s->newwline, tc);
-	tc->x = tc->prompt + ft_strlen(s->newwline);
+	tc->x = tc->neg + ft_strlen(s->newwline);
 	ft_thome(tc, s->newwline);
 	free(*line);
 	*line = s->newwline;
-	tc->x = s->i + ft_strlen(s->res) + tc->prompt;
+	tc->x = s->i + len + tc->neg;
 	return (0);
 }
