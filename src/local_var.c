f30c2cb78f2d2f0637e32c8703d885c30968b75e
Jean-yves BOBIN <jbobin@e1r6p19.42.fr>
2017-01-10 14:54:03 +0100

    FREE tes ERREURS PUTAIN

--- src/local_var.c
+++ src/local_var.c
@@ -6,7 +6,7 @@
 /*   By: chdenis <chdenis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/02 13:43:10 by chdenis           #+#    #+#             */
-/*   Updated: 2016/12/13 12:59:32 by chdenis          ###   ########.fr       */
+/*   Updated: 2017/01/10 14:52:02 by jbobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,27 @@ int					local_var_set(char *name, char *value)
 	t_localvar		*var;
 	int				result;
 
-	if (!name || !ft_strlen(name) || !ft_strisalphadigit(name))
-		return (0);
-	result = 1;
-	if (!(var = local_var_get(name)))
-		result = 2;
-	if (var || (var = (t_localvar *)ft_memalloc(sizeof(t_localvar))))
+	result = 0;
+	if (name && ft_strlen(name) && ft_strisalphadigit(name))
 	{
-		free(var->name);
-		free(var->value);
-		var->name = ft_strdup(name);
-		var->value = ft_strdup(value);
-		if (result == 2 && ((var->next = g_localvars) || 1))
-			g_localvars = var;
+		result = 1;
+		if (!(var = local_var_get(name)))
+			result = 2;
+		if (var || (var = (t_localvar *)ft_memalloc(sizeof(t_localvar))))
+		{
+			free(var->name);
+			free(var->value);
+			var->name = ft_strdup(name);
+			var->value = ft_strdup(value);
+			if (result == 2 && ((var->next = g_localvars) || 1))
+				g_localvars = var;
+		}
+		else
+			result = 0;
+		local_var_update_export(name);
 	}
-	else
-		result = 0;
-	local_var_update_export(name);
-	free(name);
-	free(value);
+	ft_strdel(&name);
+	ft_strdel(&value);
 	return (result);
 }
 
